9ac5987efc326ae098bfbc6f7a24dd2d5bacd16f
Johannes Lauinger <johannes@lauinger-it.de>
2020-04-13 13:53:49 +0200

    adapt project scraper to store repository details in CSV format

--- code/data-survey/aquisation/project-scraper.go
+++ code/data-survey/aquisation/project-scraper.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"context"
+	"encoding/csv"
 	"fmt"
+	"github.com/google/go-github/github"
+	"github.com/go-git/go-git/v5"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strconv"
 	"strings"
-
-	"github.com/google/go-github/github"
-
-	"github.com/go-git/go-git/v5"
 )
 
 func main() {
@@ -20,7 +20,20 @@ func main() {
 
 	client := github.NewClient(nil)
 
-	for page := 1; page <= 5; page++ {
+	file, err := os.Create("projects.csv")
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	writer.Write([]string{"rank", "name", "github_clone_url", "number_of_stars", "number_of_forks",
+		"github_id", "created_at", "last_pushed_at", "updated_at", "size", "checkout_path"})
+
+	for page := 1; page <= 1; page++ {
 		repos, _, err := client.Search.Repositories(context.Background(), "language:Go", &github.SearchOptions{
 			ListOptions: github.ListOptions{
 				PerPage: 100, // 100,
@@ -34,6 +47,20 @@ func main() {
 
 		for i, repo := range repos.Repositories {
 			path := downloadPath + "/" + *repo.Name
+
+			writer.Write([]string{
+				strconv.Itoa(i + 1),
+				repo.GetFullName(),
+				repo.GetCloneURL(),
+				strconv.Itoa(repo.GetStargazersCount()),
+				strconv.Itoa(repo.GetForksCount()),
+				strconv.FormatInt(*repo.ID, 10),
+				repo.CreatedAt.String(),
+				repo.PushedAt.String(),
+				repo.UpdatedAt.String(),
+				strconv.Itoa(*repo.Size),
+				path,
+			})
 
 			fmt.Printf("%v. %v\n", (page-1)*100+(i+1), *repo.CloneURL)
 			fmt.Printf("  Downloading to %v ...", path)
