bf69ac63fe3e3a06aa81266c5e2bdda22ce42f98
Johannes Lauinger <johannes@lauinger-it.de>
2020-07-07 17:36:09 +0200

    acquisition: save root module information back to projects.csv

--- code/data-survey/acquisition/projects/checkmodule.go
+++ code/data-survey/acquisition/projects/checkmodule.go
@@ -2,7 +2,9 @@ package projects
 
 import (
 	"fmt"
+	"github.com/gocarina/gocsv"
 	"github.com/stg-tud/thesis-2020-lauinger-code/data-survey/acquisition/lexical"
+	"os"
 	"os/exec"
 	"strconv"
 	"strings"
@@ -17,22 +19,34 @@ func CheckModule(dataDir string) {
 		fmt.Printf("ERROR: %v\n", err)
 	}
 
+	headerWritten := false
+	projectsFile, err := os.OpenFile(projectsFilename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+	defer projectsFile.Close()
+
 	for _, project := range projects {
 		cmd := exec.Command("go", "list", "-m")
 		cmd.Dir = project.CheckoutPath
 
-		var rootModule string
-		var usesModules bool
-
 		output, err := cmd.Output()
 		if err == nil {
-			rootModule = strings.TrimSuffix(string(output), "\n")
-			usesModules = true
+			project.RootModule = strings.TrimSuffix(string(output), "\n")
+			project.UsesModules = true
 		} else {
-			rootModule = ""
-			usesModules = false
+			project.RootModule = ""
+			project.UsesModules = false
 		}
 
-		fmt.Printf("%s: %s (%s)\n", project.Name, strconv.FormatBool(usesModules), rootModule)
+		fmt.Printf("%s: %s (%s)\n", project.Name, strconv.FormatBool(project.UsesModules), project.RootModule)
+
+		if headerWritten {
+			_ = gocsv.MarshalWithoutHeaders([]lexical.ProjectData{*project}, projectsFile)
+		} else {
+			headerWritten = true
+			_ = gocsv.Marshal([]lexical.ProjectData{*project}, projectsFile)
+		}
 	}
 }
